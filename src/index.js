deb727f23ce303cbb079088e062f8cd35af05b58
Oluwadare Oluwatobiloba <tobidechamp15@gmail.com>
2024-06-20 19:39:39 +0100

    dev changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import App from './App';
+// Create a router
+const router = createBrowserRouter([
+  {
+    path: "/",
+  },
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+root.render(<RouterProvider router={router} />);

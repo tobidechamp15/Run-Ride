13a346f143e6e4d8e561f936c065391f9e7c2ede
Oluwadare Oluwatobiloba <tobidechamp15@gmail.com>
2024-07-10 11:53:00 +0100

    dev changes

--- src/Component/Vehicles.jsx
+++ src/Component/Vehicles.jsx
@@ -10,14 +10,22 @@ const Vehicles = () => {
     <div>
       <Navbar />
       <div className="flex  flex-wrap gap-5 w-full items-center justify-center my-5">
-        <section className="flex justify-center items-center flex-col border  gap-[142px] border-black py-[48px] px-[59px]">
+        <section className="flex justify-center items-center flex-col border  gap-[24px] border-black py-[48px] px-[59px]">
           <img src={keke} alt="" />
+          <div className="flex flex-col gap-4 items-start w-full">
+            <span className="text-[16px] text-[#808080]">Napep</span>
+            <span className="text-[20px] font-semibold ">3 Passenger Ride</span>
+          </div>
           <Link className="text-white bg-black p-3 rounded-[8px] ">
             View All
           </Link>
         </section>
-        <section className="flex justify-center items-center flex-col border  gap-[142px] border-black py-[48px] px-[59px]">
+        <section className="flex justify-center items-center flex-col border  gap-[24px] border-black py-[48px] px-[59px]">
           <img src={car} alt="" />
+          <div className="flex flex-col gap-4 items-start w-full">
+            <span className="text-[16px] text-[#808080]">Cars</span>
+            <span className="text-[20px] font-semibold ">5 Passenger Ride</span>
+          </div>
           <Link
             to="/vehicles/car"
             className="text-white bg-black p-3 rounded-[8px] "
@@ -25,11 +33,15 @@ const Vehicles = () => {
             View All
           </Link>
         </section>
-        <section className="flex justify-center items-center flex-col border  gap-[142px] border-black py-[48px] px-[59px]">
+        <section className="flex justify-center items-center flex-col border  gap-[24px] border-black py-[48px] px-[59px]">
           <img src={bus} alt="" />
+          <div className="flex flex-col gap-4 items-start w-full">
+            <span className="text-[16px] text-[#808080]">Buses</span>
+            <span className="text-[20px] font-semibold ">9 Passenger Ride</span>
+          </div>
           <Link
             to="/vehicles/bus"
-            className="text-white bg-black p-3 rounded-[8px] "
+            className="text-white bg-black p-3 rounded-[8px] mt-[35px]"
           >
             View All
           </Link>
